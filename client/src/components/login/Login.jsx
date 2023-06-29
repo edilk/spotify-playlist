a56f7b39663d94e75c34e870577eed4e5f90570a
edilk <ed2803k@gmail.com>
2023-06-29 20:05:01 +0600

    URL fix

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -5,7 +5,7 @@ export const Login = () => {
     return (
         <section className="login">
             <h1>Spotify Playlist</h1>
-            <a href='http://localhost:8888/login'>Log in to spotify</a>
+            <a href={`${process.env.LOGIN_URL}${process.env.PORT}`}>Log in to spotify</a>
         </section>
     );
 }
