7b7c6bc4fec2ec080229c143bc067b395e025d84
edilk <ed2803k@gmail.com>
2023-06-30 14:32:05 +0600

    Login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -7,7 +7,7 @@ export const Login = () => {
     return (
         <section className="login">
             <h1>Spotify Playlist</h1>
-            <a href={`http://localhost:8888/login`}>Log in to spotify</a>
+            <a href={`https://spotify-playlist-edil.netlify.app/login`}>Log in to spotify</a>
         </section>
     );
 }
